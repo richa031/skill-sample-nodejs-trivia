4b3f59524d2e508db2829c6dc36fb3ca6cd54828
richa031 <richahp031@gmail.com>
2018-12-17 20:59:44 +0530

    skill samples

---
+++ lambda/custom/questions.js
@@ -0,0 +1,737 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+  QUESTIONS_EN_GB: [
+    {
+      'What protects Damon and Stefan from the sun?': [
+        'Their rings',
+        'They were turned by a doppelganger',
+        'Their bloodline',
+        'They were spelled by a witch',
+      ],
+    },
+    {
+      'What initially attracts Stefan to Elena? ': [
+        'She looked like Katherine',
+        'He was the one who saved her',
+        'He thought she was pretty',
+        'Her personality',
+      ],
+    },
+    {
+      'What does Silas do with Stefan so no one knows its him?': [
+        'Throws him in the quarry',
+        'Burns him to a crisp',
+        'Leaves him in the woods staked',
+        'Poisons him with vervain',
+      ],
+    },
+    {
+      'Who is Jeremys first girlfriend on the show?': [
+        'Vicki',
+        'Bonnie',
+        'Anna',
+        'Rebekah',
+      ],
+    },
+    {
+      'What is the name given to Lilys "friends"?': [
+        'The Heretics',
+        'The Bandits',
+        'The Groupies',
+        'The Ruckus',
+      ],
+    },
+    {
+      'Who finds Elenas journal in the cemetery?': [
+        'Stefan',
+        'Jeremy',
+        'Bonnie',
+        'Damon',
+      ],
+    },
+    {
+      'What does Klaus need in order to make hybrids?': [
+        'Elenas blood',
+        'To complete the ritual',
+        'His familys blood',
+        'His mothers blood',
+      ],
+    },
+    {
+      'Why does Stefan agree to leave town with Klaus?': [
+        'To save Damons life',
+        'Because Elena and Damon are now in love',
+        'To save Elenas life',
+        'To save his own life',
+      ],
+    },
+    {
+      'Why does Damon return to Mystic Falls?': [
+        'For Katherine',
+        'For Elena',
+        'He missed his brother',
+        'To kill Zach',
+      ],
+    },
+    {
+      'What celestial event is he planning to use to open the tomb?': [
+        'Comet',
+        'Blood Moon',
+        'Eclipse',
+        'Meteor Shower',
+      ],
+    },
+    {
+      'Who opens the tomb?': [
+        'Bonnie',
+        'Grams',
+        'Bonnie & Emily',
+        'Bonnie & Grams',
+      ],
+    },
+    {
+      'How does Carolines mom die?': [
+        'Cancer',
+        'Car accident',
+        'Shooting',
+        'Vampire attack',
+      ],
+    },
+    {
+      'Who is the mother of Alarics kids?': [
+        'Jo',
+        'Caroline',
+        'Seline',
+        'Isobel',
+      ],
+    },
+    {
+      'Where do Caroline and Stefan get married?': [
+        'The Lockwood Mansion',
+        'The Mystic Grill',
+        'Carolines Childhood Home',
+        'The Salvatore Boarding House',
+      ],
+    },
+    {
+      'Who kills Enzo?': [
+        'Stefan',
+        'Damon',
+        'Sybil',
+        'Alaric',
+      ],
+    },
+    {
+      'Who is Julian?': [
+        'Lilys boyfriend',
+        'A family member of someone Stefan killed',
+        'An old enemy',
+        'A friend of Klaus',
+      ],
+    },
+    {
+      'Who is the woman who was pregnant with Stefans child?': [
+        'Valerie',
+        'Mary Louise',
+        'Katherine',
+        'Nora',
+      ],
+    },
+    {
+      'What career does Elena end up doing?': [
+        'Doctor',
+        'Writer',
+        'Model',
+        'Nurse',
+      ],
+    },
+    {
+      'Who kills Mason Lockwood?': [
+        'Damon',
+        'Katherine',
+        'Stefan',
+        'Klaus',
+      ],
+    },
+    {
+      'What is the name of the stone that is needed to break Klaus curse?': [
+        'Moon Stone',
+        'Werewolf Stone',
+        'Transformation Stone',
+        'Sun Stone',
+      ],
+    },
+    {
+      'How does the siren control Damon?': [
+        'She alters his memories of Elena',
+        'Elenas voice',
+        'She changes her appearance to look like Elena',
+        'She threatens Stefan',
+      ],
+    },
+    {
+      'Who burns down the Gilbert house?': [
+        'Elena',
+        'Katherine',
+        'Kol',
+        'Klaus',
+      ],
+    },
+    {
+      'Why does Elena turn off her humanity?': [
+        'Jeremys death',
+        'Bonnies death',
+        'Her feelings for Damon',
+        'She felt bad for breaking Stefans heart',
+      ],
+    },
+    {
+      'In the 1964 movie, why is the train on the Island of Misfit Toys?': [
+        'Square wheels',
+        'No Engine',
+        'Paint does not match',
+        'It does not toot',
+      ],
+    },
+    {
+      'Why does Bonnie start college later than the other girls?': [
+        'She was dead',
+        'She was visiting family',
+        'She was traveling',
+        'She was spending time with Jeremy',
+
+      ],
+    },
+    {
+      'Which Salvatore brother dies at the end of the series?': [
+        'Stefan',
+        'Neither',
+        'Both',
+        'Damon',
+      ],
+    },
+    {
+      'Who ends up taking the cure first?': [
+        'Katherine',
+        'Rebekah',
+        'Stefan',
+        'Elena',
+      ],
+    },
+    {
+      'Who turns Alaric into a vampire?': [
+        'Esther',
+        'Klaus',
+        'Damon',
+        'Elijah',
+      ],
+    },
+    {
+      'Who saves Alaric when he crosses the border and is stripped of his vampirism?': [
+        'Jo',
+        'Elena',
+        'Caroline',
+        'Bonnie',
+      ],
+    },
+    {
+      'Who does Jeremy stay with when Elenas at college?': [
+        'Damon',
+        'Tyler',
+        'Stefan',
+        'Liz',
+      ],
+    },
+  ],
+  QUESTIONS_EN_US: [
+    {
+      'What protects Damon and Stefan from the sun?': [
+        'Their rings',
+        'They were turned by a doppelganger',
+        'Their bloodline',
+        'They were spelled by a witch',
+      ],
+    },
+    {
+      'What initially attracts Stefan to Elena? ': [
+        'She looked like Katherine',
+        'He was the one who saved her',
+        'He thought she was pretty',
+        'Her personality',
+      ],
+    },
+    {
+      'What does Silas do with Stefan so no one knows its him?': [
+        'Throws him in the quarry',
+        'Burns him to a crisp',
+        'Leaves him in the woods staked',
+        'Poisons him with vervain',
+      ],
+    },
+    {
+      'Who is Jeremys first girlfriend on the show?': [
+        'Vicki',
+        'Bonnie',
+        'Anna',
+        'Rebekah',
+      ],
+    },
+    {
+      'What is the name given to Lilys "friends"?': [
+        'The Heretics',
+        'The Bandits',
+        'The Groupies',
+        'The Ruckus',
+      ],
+    },
+    {
+      'Who finds Elenas journal in the cemetery?': [
+        'Stefan',
+        'Jeremy',
+        'Bonnie',
+        'Damon',
+      ],
+    },
+    {
+      'What does Klaus need in order to make hybrids?': [
+        'Elenas blood',
+        'To complete the ritual',
+        'His familys blood',
+        'His mothers blood',
+      ],
+    },
+    {
+      'Why does Stefan agree to leave town with Klaus?': [
+        'To save Damons life',
+        'Because Elena and Damon are now in love',
+        'To save Elenas life',
+        'To save his own life',
+      ],
+    },
+    {
+      'Why does Damon return to Mystic Falls?': [
+        'For Katherine',
+        'For Elena',
+        'He missed his brother',
+        'To kill Zach',
+      ],
+    },
+    {
+      'What celestial event is he planning to use to open the tomb?': [
+        'Comet',
+        'Blood Moon',
+        'Eclipse',
+        'Meteor Shower',
+      ],
+    },
+    {
+      'Who opens the tomb?': [
+        'Bonnie',
+        'Grams',
+        'Bonnie & Emily',
+        'Bonnie & Grams',
+      ],
+    },
+    {
+      'How does Carolines mom die?': [
+        'Cancer',
+        'Car accident',
+        'Shooting',
+        'Vampire attack',
+      ],
+    },
+    {
+      'Who is the mother of Alarics kids?': [
+        'Jo',
+        'Caroline',
+        'Seline',
+        'Isobel',
+      ],
+    },
+    {
+      'Where do Caroline and Stefan get married?': [
+        'The Lockwood Mansion',
+        'The Mystic Grill',
+        'Carolines Childhood Home',
+        'The Salvatore Boarding House',
+      ],
+    },
+    {
+      'Who kills Enzo?': [
+        'Stefan',
+        'Damon',
+        'Sybil',
+        'Alaric',
+      ],
+    },
+    {
+      'Who is Julian?': [
+        'Lilys boyfriend',
+        'A family member of someone Stefan killed',
+        'An old enemy',
+        'A friend of Klaus',
+      ],
+    },
+    {
+      'Who is the woman who was pregnant with Stefans child?': [
+        'Valerie',
+        'Mary Louise',
+        'Katherine',
+        'Nora',
+      ],
+    },
+    {
+      'What career does Elena end up doing?': [
+        'Doctor',
+        'Writer',
+        'Model',
+        'Nurse',
+      ],
+    },
+    {
+      'Who kills Mason Lockwood?': [
+        'Damon',
+        'Katherine',
+        'Stefan',
+        'Klaus',
+      ],
+    },
+    {
+      'What is the name of the stone that is needed to break Klaus curse?': [
+        'Moon Stone',
+        'Werewolf Stone',
+        'Transformation Stone',
+        'Sun Stone',
+      ],
+    },
+    {
+      'How does the siren control Damon?': [
+        'She alters his memories of Elena',
+        'Elenas voice',
+        'She changes her appearance to look like Elena',
+        'She threatens Stefan',
+      ],
+    },
+    {
+      'Who burns down the Gilbert house?': [
+        'Elena',
+        'Katherine',
+        'Kol',
+        'Klaus',
+      ],
+    },
+    {
+      'Why does Elena turn off her humanity?': [
+        'Jeremys death',
+        'Bonnies death',
+        'Her feelings for Damon',
+        'She felt bad for breaking Stefans heart',
+      ],
+    },
+    {
+      'In the 1964 movie, why is the train on the Island of Misfit Toys?': [
+        'Square wheels',
+        'No Engine',
+        'Paint does not match',
+        'It does not toot',
+      ],
+    },
+    {
+      'Why does Bonnie start college later than the other girls?': [
+        'She was dead',
+        'She was visiting family',
+        'She was traveling',
+        'She was spending time with Jeremy',
+
+      ],
+    },
+    {
+      'Which Salvatore brother dies at the end of the series?': [
+        'Stefan',
+        'Neither',
+        'Both',
+        'Damon',
+      ],
+    },
+    {
+      'Who ends up taking the cure first?': [
+        'Katherine',
+        'Rebekah',
+        'Stefan',
+        'Elena',
+      ],
+    },
+    {
+      'Who turns Alaric into a vampire?': [
+        'Esther',
+        'Klaus',
+        'Damon',
+        'Elijah',
+      ],
+    },
+    {
+      'Who saves Alaric when he crosses the border and is stripped of his vampirism?': [
+        'Jo',
+        'Elena',
+        'Caroline',
+        'Bonnie',
+      ],
+    },
+    {
+      'Who does Jeremy stay with when Elenas at college?': [
+        'Damon',
+        'Tyler',
+        'Stefan',
+        'Liz',
+      ],
+    },
+  ],
+  QUESTIONS_EN_IN: [
+    {
+      'What protects Damon and Stefan from the sun?': [
+        'Their rings',
+        'They were turned by a doppelganger',
+        'Their bloodline',
+        'They were spelled by a witch',
+      ],
+    },
+    {
+      'What initially attracts Stefan to Elena? ': [
+        'She looked like Katherine',
+        'He was the one who saved her',
+        'He thought she was pretty',
+        'Her personality',
+      ],
+    },
+    {
+      'What does Silas do with Stefan so no one knows its him?': [
+        'Throws him in the quarry',
+        'Burns him to a crisp',
+        'Leaves him in the woods staked',
+        'Poisons him with vervain',
+      ],
+    },
+    {
+      'Who is Jeremys first girlfriend on the show?': [
+        'Vicki',
+        'Bonnie',
+        'Anna',
+        'Rebekah',
+      ],
+    },
+    {
+      'What is the name given to Lilys "friends"?': [
+        'The Heretics',
+        'The Bandits',
+        'The Groupies',
+        'The Ruckus',
+      ],
+    },
+    {
+      'Who finds Elenas journal in the cemetery?': [
+        'Stefan',
+        'Jeremy',
+        'Bonnie',
+        'Damon',
+      ],
+    },
+    {
+      'What does Klaus need in order to make hybrids?': [
+        'Elenas blood',
+        'To complete the ritual',
+        'His familys blood',
+        'His mothers blood',
+      ],
+    },
+    {
+      'Why does Stefan agree to leave town with Klaus?': [
+        'To save Damons life',
+        'Because Elena and Damon are now in love',
+        'To save Elenas life',
+        'To save his own life',
+      ],
+    },
+    {
+      'Why does Damon return to Mystic Falls?': [
+        'For Katherine',
+        'For Elena',
+        'He missed his brother',
+        'To kill Zach',
+      ],
+    },
+    {
+      'What celestial event is he planning to use to open the tomb?': [
+        'Comet',
+        'Blood Moon',
+        'Eclipse',
+        'Meteor Shower',
+      ],
+    },
+    {
+      'Who opens the tomb?': [
+        'Bonnie',
+        'Grams',
+        'Bonnie & Emily',
+        'Bonnie & Grams',
+      ],
+    },
+    {
+      'How does Carolines mom die?': [
+        'Cancer',
+        'Car accident',
+        'Shooting',
+        'Vampire attack',
+      ],
+    },
+    {
+      'Who is the mother of Alarics kids?': [
+        'Jo',
+        'Caroline',
+        'Seline',
+        'Isobel',
+      ],
+    },
+    {
+      'Where do Caroline and Stefan get married?': [
+        'The Lockwood Mansion',
+        'The Mystic Grill',
+        'Carolines Childhood Home',
+        'The Salvatore Boarding House',
+      ],
+    },
+    {
+      'Who kills Enzo?': [
+        'Stefan',
+        'Damon',
+        'Sybil',
+        'Alaric',
+      ],
+    },
+    {
+      'Who is Julian?': [
+        'Lilys boyfriend',
+        'A family member of someone Stefan killed',
+        'An old enemy',
+        'A friend of Klaus',
+      ],
+    },
+    {
+      'Who is the woman who was pregnant with Stefans child?': [
+        'Valerie',
+        'Mary Louise',
+        'Katherine',
+        'Nora',
+      ],
+    },
+    {
+      'What career does Elena end up doing?': [
+        'Doctor',
+        'Writer',
+        'Model',
+        'Nurse',
+      ],
+    },
+    {
+      'Who kills Mason Lockwood?': [
+        'Damon',
+        'Katherine',
+        'Stefan',
+        'Klaus',
+      ],
+    },
+    {
+      'What is the name of the stone that is needed to break Klaus curse?': [
+        'Moon Stone',
+        'Werewolf Stone',
+        'Transformation Stone',
+        'Sun Stone',
+      ],
+    },
+    {
+      'How does the siren control Damon?': [
+        'She alters his memories of Elena',
+        'Elenas voice',
+        'She changes her appearance to look like Elena',
+        'She threatens Stefan',
+      ],
+    },
+    {
+      'Who burns down the Gilbert house?': [
+        'Elena',
+        'Katherine',
+        'Kol',
+        'Klaus',
+      ],
+    },
+    {
+      'Why does Elena turn off her humanity?': [
+        'Jeremys death',
+        'Bonnies death',
+        'Her feelings for Damon',
+        'She felt bad for breaking Stefans heart',
+      ],
+    },
+    {
+      'In the 1964 movie, why is the train on the Island of Misfit Toys?': [
+        'Square wheels',
+        'No Engine',
+        'Paint does not match',
+        'It does not toot',
+      ],
+    },
+    {
+      'Why does Bonnie start college later than the other girls?': [
+        'She was dead',
+        'She was visiting family',
+        'She was traveling',
+        'She was spending time with Jeremy',
+
+      ],
+    },
+    {
+      'Which Salvatore brother dies at the end of the series?': [
+        'Stefan',
+        'Neither',
+        'Both',
+        'Damon',
+      ],
+    },
+    {
+      'Who ends up taking the cure first?': [
+        'Katherine',
+        'Rebekah',
+        'Stefan',
+        'Elena',
+      ],
+    },
+    {
+      'Who turns Alaric into a vampire?': [
+        'Esther',
+        'Klaus',
+        'Damon',
+        'Elijah',
+      ],
+    },
+    {
+      'Who saves Alaric when he crosses the border and is stripped of his vampirism?': [
+        'Jo',
+        'Elena',
+        'Caroline',
+        'Bonnie',
+      ],
+    },
+    {
+      'Who does Jeremy stay with when Elenas at college?': [
+        'Damon',
+        'Tyler',
+        'Stefan',
+        'Liz',
+      ],
+    },
+  ],
+};
